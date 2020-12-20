b0918e881188364736e495a5456ba171b5285add
Éric Noyau <noyau@google.com>
2020-12-20 17:34:22 +0100

    Pick a library, add some constants.

---
+++ messagebox/config_template.h
@@ -0,0 +1,15 @@
+
+// Duplicate this file into config.h and fill the blanks according to your needs.
+
+// How to connect to WiFi
+#define WIFI_SSID "################"
+#define WIFI_PASSWORD "################"
+
+/* The imap host name e.g. imap.gmail.com for GMail or outlook.office365.com for Outlook */
+#define IMAP_HOST "imap.gmail.com"
+#define IMAP_PORT 993
+
+/* Login. For gmail use an app password */
+#define EMAIL "###@############"
+#define IMAP_PASSWORD "#############"
+
